53b0c7b026da3716efa4f4af68e7ddbd4b463874
Rajnish Katharotiya <katharotiyarajnish@gmail.com>
2021-08-23 23:22:37 -0400

    Updated filter and recipe detail card design

---
+++ src/Components/Recipes/RecipeDetailCard.js
@@ -0,0 +1,71 @@
+
+import { useEffect, useState } from "react";
+import { Spinner, Button } from "react-bootstrap";
+import Modal from 'react-bootstrap/Modal';
+
+import './RecipeDetailCard.css';
+
+const RecipeDetailCard = ({ id, show, title, handleUpdateStorage, fav, cart, ...props }) => {
+
+    const [detail, setDetail] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    const fetchRecipeDetail = async () => {
+        if (!id) {
+            return;
+        }
+        try {
+            setLoading(true);
+            const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+            const result = await res.json();
+            setDetail(result.meals[0]);
+            console.log(result.meals[0])
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if (show) {
+            fetchRecipeDetail();
+        }
+    }, [show])
+
+    const ingredients = Object.keys(detail).filter(e => e.includes('strIngredient')) || [];
+
+    return (
+        <Modal
+            {...props}
+            show={show}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            className="recipe-detail_modal-card"
+        >
+            <Modal.Body className="recipe-detail-card_body">
+                {loading ? <div className="recipe-detail-card_box"><Spinner animation="grow" /></div> :
+                    <div className="recipe-detail-card_content">
+                        <div className="recipe-detail-card_content-info">
+                            <h4>{title}</h4>
+                            <h6 className="recipe-detail_title">Ingredients :</h6>
+                            <div className="recipe-detail-card_ingredients">
+                                {ingredients.map(e => detail[e] && <p>{detail[e]} <span className="recipe-detail-card_measure">({detail[`strMeasure${parseInt(e.replace('strIngredient', ''), 10)}`]})</span><span className="divider">,</span></p>)}
+                            </div>
+                            <h6 className="recipe-detail_title">Preparation :</h6>
+                            <p>
+                                {detail?.strInstructions}
+                            </p>
+                        </div>
+                        <img src={detail.strMealThumb} alt={title} className="recipe-detail-card_content-img" />
+                        <div className="recipe-detail_action-btn">
+                            <Button variant="danger" className="action-button" onClick={e => handleUpdateStorage('favItems')}><i className={`bi bi-heart${fav ? '-fill' : ''}`}></i>{fav ? 'Added' : 'Add'} to favorite</Button>
+                            <Button variant="success" className="action-button" onClick={e => handleUpdateStorage('cartItems')}><i className={`bi bi-cart${cart ? '-fill' : ''}`}></i>{cart ? 'Added' : 'Add'} to cart</Button>
+                        </div>
+                    </div>
+                }
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+export default RecipeDetailCard;
